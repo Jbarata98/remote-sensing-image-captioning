977aac189a75df59c95a89f2340f3ebba6b506b2
Joao Barata <jbarata1998@gmail.com>
2021-03-15 20:18:11 +0000

    classification v2

--- encoder_scripts/train_encoder.py
+++ encoder_scripts/train_encoder.py
@@ -1,13 +1,15 @@
+
 from models.base_model import Encoder
 from configs.utils import *
 from encoder_scripts.encoder_training_details import *
-from encoder_scripts.create_classification_data import create_classification_files,create_classes_json
-import sys
+from encoder_scripts.create_classification_data import create_classification_files, create_classes_json
+
 import os
+
 
 class finetune():
 
-    def __init__(self, model_type, device, nr_classes=31, enable_finetuning=True): #rsicd is 31 classes
+    def __init__(self, model_type, device, nr_classes=31, enable_finetuning=True):  # rsicd is 31 classes
         self.device = device
         logging.info("Running encoder fine-tuning script...")
 
@@ -15,39 +17,55 @@ class finetune():
         self.classes = nr_classes
         self.enable_finetuning = enable_finetuning
         self.checkpoint_exists = False
+        self.device = device
+
+        image_model, dim = get_encoder_model(self.model_type)
+        image_model._fc = nn.Linear(dim, self.classes)
+
+        self.model = image_model.to(self.device)
 
     def _setup_train(self):
+
+        optimizer = get_optimizer(OPTIMIZER)(
+            params=filter(lambda p: p.requires_grad, self.model.parameters()),
+            lr=encoder_lr) if self.enable_finetuning else None
+
+        self.optimizer = optimizer
+
+        self.criterion = get_loss_function(LOSS)
+
+        self._load_weights_from_checkpoint(load_to_train=True)
+
+    def _load_weights_from_checkpoint(self, load_to_train):
+
         if os.path.exists(checkpoint_encoder_path):
             logging.info("checkpoint exists, loading...")
             checkpoint = torch.load(checkpoint_encoder_path)
+            self.checkpoint_exists = True
 
-            self.checkpoint_start_epoch = checkpoint['epoch'] + 1
-            self.checkpoint_epochs_since_last_improvement = checkpoint['epochs_since_improvement']
-            self.checkpoint_val_loss = checkpoint['val_loss']
+            checkpoint = torch.load(checkpoint_encoder_path)
 
-            #load weights for encoder
-            image_model = checkpoint['model']
-            optimizer = checkpoint_encoder_path['optimizer']
+            # load model weights
+            self.model.load_state_dict(checkpoint['model'])
 
-            logging.info(
-                "Restore model from checkpoint. Start epoch %s ", self.checkpoint_start_epoch)
-        else:
-            logging.info("checkpoint does not exist... starting from the start")
-            image_model,dim = get_encoder_model(self.model_type)
-            image_model._fc = nn.Linear(dim, self.classes)
+            if load_to_train:
+                # load optimizers and start epoch
+                self.checkpoint_start_epoch = checkpoint['epoch'] + 1
+                self.checkpoint_epochs_since_last_improvement = checkpoint['epochs_since_improvement']
+                self.checkpoint_val_loss = checkpoint['val_loss']
 
-            optimizer = get_optimizer(OPTIMIZER)(
-                params=filter(lambda p: p.requires_grad, image_model.parameters()),
-                lr=encoder_lr) if self.enable_finetuning else None
-        self.model = image_model.to(device)
-        self.optimizer = optimizer
-        self.criterion = get_loss_function(LOSS)
+                # load weights for encoder
+                self.optimizer.load_state_dict(checkpoint['optimizer'])
 
+                logging.info(
+                    "Restore model from checkpoint. Start epoch %s ", self.checkpoint_start_epoch)
+            else:
+                logging.info(
+                    "No checkpoint. Will start model from beggining\n")
 
     def _train_step(self, imgs, targets):
         imgs = imgs.to(self.device)
         targets = targets.to(self.device)
-        print(imgs.shape)
         outputs = self.model(imgs)
 
         targets = targets.squeeze(1)
@@ -76,7 +94,7 @@ class finetune():
             epochs_since_last_improvement=self.checkpoint_epochs_since_last_improvement
             if self.checkpoint_exists else 0,
             baseline=self.checkpoint_val_loss if self.checkpoint_exists else np.Inf,
-            encoder_optimizer= self.optimizer,  # TENS
+            encoder_optimizer=self.optimizer,  # TENS
             decoder_optimizer=None,
             period_decay_lr=2  # no decay lr!
         )
@@ -144,9 +162,9 @@ class finetune():
             early_stopping.check_improvement(epoch_val_loss)
 
             self._save_checkpoint_encoder(early_stopping.is_current_val_best(),
-                                  epoch,
-                                  early_stopping.get_number_of_epochs_without_improvement(),
-                                  epoch_val_loss)
+                                          epoch,
+                                          early_stopping.get_number_of_epochs_without_improvement(),
+                                          epoch_val_loss)
 
             logging.info(
                 '\n-------------- END EPOCH:{}⁄{}; Train Loss:{:.4f}; Val Loss:{:.4f} -------------\n'.format(
@@ -161,29 +179,24 @@ class finetune():
                 )
             )
 
-    def _save_checkpoint_encoder(self, epoch, epochs_since_improvement, val_loss,
-                                is_best):
+    def _save_checkpoint_encoder(self, val_loss_improved, epoch, epochs_since_improvement, val_loss
+                                 ):
+        if val_loss_improved:
+            state = {'epoch': epoch,
+                     'epochs_since_improvement': epochs_since_improvement,
+                     'val_loss': val_loss,
+                     'model': self.model.state_dict(),
+                     'optimizer': self.optimizer.state_dict()
+                     }
 
-        state = {'epoch': epoch,
-                 'epochs_since_last_improvement': epochs_since_improvement,
-                 'val_loss': val_loss,
-                 'model': self.model.state_dict(),
-                 'optimizer': self.optimizer.state_dict()
-                 }
-
-        filename_checkpoint = get_path(model=ENCODER_MODEL, is_encoder=True)
-        torch.save(state, filename_checkpoint)
-        # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
-        if is_best:
-            filename_best_checkpoint = get_path(model=ENCODER_MODEL, best_checkpoint=True, is_encoder=True)
-            torch.save(state, filename_best_checkpoint)
+            filename_checkpoint = get_path(model=ENCODER_MODEL, is_encoder=True)
+            torch.save(state, filename_checkpoint)
+            # If this checkpoint is the best so far, store a copy so it doesn't get overwritten by a worse checkpoint
 
 
 if __name__ == "__main__":
-
     logging.basicConfig(
         format='%(levelname)s: %(message)s', level=logging.INFO)
-
 
     logging.info("Device: %s \nCount %i gpus",
                  device, torch.cuda.device_count())
@@ -202,9 +215,8 @@ if __name__ == "__main__":
         ClassificationDataset(data_folder, data_name, 'VAL', transform=transforms.Compose([normalize])),
         batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
 
-
     #
-    model = finetune(model_type= ENCODER_MODEL,device = device)
+    model = finetune(model_type=ENCODER_MODEL, device=device)
     model._setup_train()
     model.train(train_loader, val_loader)
 
