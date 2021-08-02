84a419a9f07eaef0804732c987daf91117f5977a
Joao Barata <jbarata1998@gmail.com>
2021-08-02 10:14:04 +0100

    get data, compute metric

---
+++ src/captioning_scripts/fusion/pegasus/pretrain_pegasus.py
@@ -0,0 +1,132 @@
+"""Script for fine-tuning Pegasus
+
+  #Script to fine-tune Pegasus for captioning task
+  - Similar images captions as input,
+  - target is a random sentence from the 5
+
+
+  # use Pegasus xsum model as base for fine-tuning
+  model_name = 'google/pegasus-large'
+  train_dataset, _, _, tokenizer = prepare_data(model_name, train_texts, train_labels)
+  trainer = prepare_fine_tuning(model_name, tokenizer, train_dataset)
+  trainer.train()
+
+Reference:
+  https://gist.github.com/jiahao87/50cec29725824da7ff6dd9314b53c4b3
+"""
+
+import torch
+import json
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer, Trainer, TrainingArguments
+from src.configs.setters.set_initializers import *
+
+
+setters = Setters(file='configs/setters/training_details.txt')
+
+paths = setters._set_paths()
+
+class PegasusFinetuneDataset(torch.utils.data.Dataset):
+    """
+    Pegasus Dataset specific for fine-tuning task
+    """
+    def __init__(self, encodings, targets):
+        self.encodings = encodings
+        self.target = targets
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels['input_ids'][idx])  # torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels['input_ids'])  # len(self.labels)
+
+def get_data(filename, split):
+    """
+    gets raw data, shuffles it and concatenates
+    extracts label (target sentence is a random one from the 5)
+    """
+
+def prepare_data(model_name,
+                 train_texts, train_labels,
+                 val_texts=None, val_labels=None,
+                 test_texts=None, test_labels=None):
+    """
+  Prepare input data for model fine-tuning
+  """
+    tokenizer = PegasusTokenizer.from_pretrained(model_name)
+
+    prepare_val = False if val_texts is None or val_labels is None else True
+    prepare_test = False if test_texts is None or test_labels is None else True
+
+    def tokenize_data(texts, labels):
+        encodings = tokenizer(texts, truncation=True, padding='longest')
+        decodings = tokenizer(labels, truncation=True, padding='longest')
+        dataset_tokenized = PegasusFinetuneDataset(encodings, decodings)
+        return dataset_tokenized
+
+    train_dataset = tokenize_data(train_texts, train_labels)
+    val_dataset = tokenize_data(val_texts, val_labels) if prepare_val else None
+    test_dataset = tokenize_data(test_texts, test_labels) if prepare_test else None
+
+    return train_dataset, val_dataset, test_dataset, tokenizer
+
+#def compute_metrics
+#computes bleu-4
+
+def prepare_fine_tuning(model_name, tokenizer, train_dataset, val_dataset=None, freeze_encoder=False, output_dir='./results'):
+  """
+  Prepare configurations and base model for fine-tuning
+  """
+  torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+  model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+
+  if freeze_encoder:
+    for param in model.model.encoder.parameters():
+      param.requires_grad = False
+
+  if val_dataset is not None:
+    training_args = TrainingArguments(
+      output_dir=output_dir,           # output directory
+      num_train_epochs=2000,           # total number of training epochs
+      per_device_train_batch_size=1,   # batch size per device during training, can increase if memory allows
+      per_device_eval_batch_size=1,    # batch size for evaluation, can increase if memory allows
+      save_steps=500,                  # number of updates steps before checkpoint saves
+      save_total_limit=5,              # limit the total amount of checkpoints and deletes the older checkpoints
+      evaluation_strategy='steps',     # evaluation strategy to adopt during training
+      eval_steps=100,                  # number of update steps before evaluation
+      warmup_steps=500,                # number of warmup steps for learning rate scheduler
+      weight_decay=0.01,               # strength of weight decay
+      logging_dir='./logs',            # directory for storing logs
+      logging_steps=10,
+    )
+
+    trainer = Trainer(
+      model=model,                         # the instantiated 🤗 Transformers model to be trained
+      args=training_args,                  # training arguments, defined above
+      train_dataset=train_dataset,         # training dataset
+      eval_dataset=val_dataset,            # evaluation dataset
+      tokenizer=tokenizer
+    )
+
+  else:
+    training_args = TrainingArguments(
+      output_dir=output_dir,           # output directory
+      num_train_epochs=2000,           # total number of training epochs
+      per_device_train_batch_size=1,   # batch size per device during training, can increase if memory allows
+      save_steps=500,                  # number of updates steps before checkpoint saves
+      save_total_limit=5,              # limit the total amount of checkpoints and deletes the older checkpoints
+      warmup_steps=500,                # number of warmup steps for learning rate scheduler
+      weight_decay=0.01,               # strength of weight decay
+      logging_dir='./logs',            # directory for storing logs
+      logging_steps=10,
+    )
+
+    trainer = Trainer(
+      model=model,                         # the instantiated 🤗 Transformers model to be trained
+      args=training_args,                  # training arguments, defined above
+      train_dataset=train_dataset,         # training dataset
+      tokenizer=tokenizer
+    )
+
+  return trainer
