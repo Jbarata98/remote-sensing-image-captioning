6b6508070f5c9aaac74e20b8661bcc077d6c5208
Joao Barata <jbarata1998@gmail.com>
2021-08-29 21:56:14 +0100

    SupConEffNet, eff_net v2 functionality added

--- src/classification_scripts/SupConLoss/SupConModel.py
+++ src/classification_scripts/SupConLoss/SupConModel.py
@@ -1,3 +1,5 @@
+import timm
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from efficientnet_pytorch import EfficientNet
@@ -6,10 +8,17 @@ from efficientnet_pytorch import EfficientNet
 class SupConEffNet(nn.Module):
     """backbone + projection head"""
 
-    def __init__(self, head='mlp', feat_dim=128):
+    def __init__(self, eff_net_version = 'v1', head='mlp', feat_dim=128):
         super(SupConEffNet, self).__init__()
-        self.model = EfficientNet.from_pretrained('efficientnet-b5')
-        self.encoder_dim = self.model._fc.in_features
+        self.eff_net_version = eff_net_version
+        """---------EFF_NET VERSION----------------"""
+        if self.eff_net_version == 'v1':
+            self.model = EfficientNet.from_pretrained('efficientnet-b5')
+            self.encoder_dim = self.model._fc.in_features
+        elif self.eff_net_version =='v2':
+            self.model = timm.create_model('tf_efficientnetv2_m_in21k',pretrained=True)
+            self.encoder_dim = self.model.forward_features(torch.randn(1, 3, 224, 224)).shape[1]
+
         if head == 'linear':
             self.head = nn.Linear(self.encoder_dim, feat_dim)
         elif head == 'mlp':
@@ -23,7 +32,10 @@ class SupConEffNet(nn.Module):
                 'head not supported: {}'.format(head))
 
     def forward(self, x):
-        feat = self.model.extract_features(x)
+        if self.eff_net_version == 'v1':
+            feat = self.model.extract_features(x)
+        elif self.eff_net_version =='v2':
+            feat = self.model.forward_features(x)
         feat = feat.permute(0, 2, 3, 1).flatten(start_dim = 1, end_dim=2).mean(dim=1)
 
         feat = F.normalize(self.head(feat), dim=1)
@@ -33,10 +45,18 @@ class SupConEffNet(nn.Module):
 class LinearClassifier(nn.Module):
     """Linear classifier"""
 
-    def __init__(self, num_classes=31):
+    def __init__(self,eff_net_version = 'v1', num_classes=31):
         super(LinearClassifier, self).__init__()
-        image_model = EfficientNet.from_pretrained('efficientnet-b5')
-        encoder_dim = image_model._fc.in_features
+
+        self.eff_net_version = eff_net_version
+
+        if self.eff_net_version == 'v1':
+            image_model = EfficientNet.from_pretrained('efficientnet-b5')
+            encoder_dim = image_model._fc.in_features
+        elif self.eff_net_version =='v2':
+            self.model = timm.create_model('tf_efficientnetv2_m_in21k', pretrained=True)
+            self.encoder_dim = self.model.forward_features(torch.randn(1, 3, 224, 224)).shape[1]
+
         self.fc = nn.Linear(encoder_dim, num_classes)
 
     def forward(self, features):
--- src/configs/globals.py
+++ src/configs/globals.py
@@ -7,7 +7,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
 
 # task {Retrieval,Classification,Captioning,Summarization}
-TASK = 'Captioning'
+TASK = 'Classification'
 
 # if using COLAB
 COLAB = False
@@ -23,13 +23,13 @@ SPECIAL_TOKENS = {"bos_token": "<start>",
                   "pad_token": "<pad>"}
 
 # GLOBAL PARAMETERS
-ARCHITECTURE = ARCHITECTURES.BASELINE.value
+ARCHITECTURE = ARCHITECTURES.FUSION.value
 DATASET = DATASETS.RSICD.value
 
 CUSTOM_VOCAB = True  # True if creating a custom vocab in order to reduce the size.
 
 # MODELS
-ENCODER_MODEL = ENCODERS.EFFICIENT_NET_IMAGENET.value  # which encoder using now
+ENCODER_MODEL = ENCODERS.EFFICIENT_NET_IMAGENET_FINETUNED_AUGMENTED_CONTRASTIVE.value  # which encoder using now
 
 AUX_LM = AUX_LMs.PEGASUS.value if ARCHITECTURE == ARCHITECTURES.FUSION.value else None  # which aux. LM using
 
