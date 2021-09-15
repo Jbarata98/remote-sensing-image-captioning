9ed144970d9586098ee0761b1973467de93412bf
Joao Barata <jbarata1998@gmail.com>
2021-09-15 19:30:30 +0100

    Add types of fusion

--- src/configs/globals.py
+++ src/configs/globals.py
@@ -32,7 +32,7 @@ CUSTOM_VOCAB = True  # True if creating a custom vocab in order to reduce the si
 
 """------------------------------------------------ MODELS ----------------------------------------------------------"""
 # MODELS
-ENCODER_MODEL = ENCODERS.EFFICIENT_NET_V2_IMAGENET_FINETUNED_AUGMENTED_CONTRASTIVE_ALS.value  # which encoder using now
+ENCODER_MODEL = ENCODERS.EFFICIENT_NET_V2_IMAGENET_FINETUNED_AUGMENTED_CONTRASTIVE.value  # which encoder using now
 
 AUX_LM = AUX_LMs.PEGASUS.value if ARCHITECTURE == ARCHITECTURES.FUSION.value else None  # which aux. LM using
 
@@ -47,6 +47,12 @@ if AUX_LM == AUX_LMs.PEGASUS.value:
     # if doing multi_input for pegasus encoder else False
     MULTI_INPUT = False
     REDUCTION_LAYER = True
+    """--------Types of Fusion--------"""
+    CONCAT_ONLY = False
+    SIMPLE_FUSION = True
+    #todo
+    COLD_FUSION = False
+    HIERARCHICAL_FUSION = False
 
 if TASK == 'Classification':
     EXTRA_EPOCHS = True
@@ -80,4 +86,4 @@ bleurt_checkpoint = "metrics_files/bleurt/test_checkpoint"  # uses Tiny
 # LOADERS
 # which pre-trained encoder loading from/loading to
 # if doing classification pretraining  the loader path might be different from the current encoder (pretraining an efficientnet on imagenet)
-ENCODER_LOADER = ENCODERS.EFFICIENT_NET_V2_IMAGENET_FINETUNED_AUGMENTED_CONTRASTIVE_ALS.value if TASK == 'Classification' else ENCODER_MODEL
+ENCODER_LOADER = ENCODERS.EFFICIENT_NET_V2_IMAGENET_FINETUNED_AUGMENTED_CONTRASTIVE.value if TASK == 'Classification' else ENCODER_MODEL
